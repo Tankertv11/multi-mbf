c93da8112b1c9b705e75b64c696bf57cfe0eec6a
Tankertv11 <66780001+Tankertv11@users.noreply.github.com>
2020-06-16 23:46:00 +0700

    Update logo.py

--- lib/logo.py
+++ lib/logo.py
@@ -1,4 +1,17 @@
-#Encode by Anbia Nuansyah
-#Apa Kau mau recode ?
-import base64
-exec(base64.b64decode('aW1wb3J0IG9zCncgPSAnXHgxYlsxOzM3bScKYiA9ICdceDFiWzE7MzZtJwpnID0gJ1x4MWJbMTszMm0nCnkgPSAnXHgxYlsxOzMzbScKciA9ICdceDFiWzE7MzFtJwoKZGVmIGJhbm5lcigpOgoJb3Muc3lzdGVtKCdjbGVhcicpCglwcmludChmJycne2d94pWU4pWm4pWX4pSsIOKUrOKUrCDilIzilKzilJDilKwgICDilZTilZcg4pWU4pWQ4pWXCuKVkeKVkeKVkeKUgiDilILilIIgIOKUgiDilILilIDilIDilIDilaDilanilZfilaDilaMK4pWpIOKVqeKUlOKUgOKUmOKUtOKUgOKUmOKUtCDilLQgICDilZrilZDilZ3ilZp7eX0gdjAuMQp7d33ilZTilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZcKe3d94pWRe3l9Knt3fSBBdXRob3J7cn0gOntifSBBbmJpYSBOdWFuc3lhaCAgICAgICAgICAgICB7d30gICDilZEKe3d94pWRe3l9Knt3fSBZb3V0dWJle3J9OntifSBTQU5TQkFFICAgICAgICAgICAgICAge3d9ICAgICAgICDilZEKe3d94pWRe3l9Knt3fSBHaXRodWJ7cn0gOntnfSBceDFiWzRtaHR0cHM6Ly9naXRodWIuY29tL1NhbnNCYWVceDFiWzBte3d9ICAgIOKVkQp7d33ilZrilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZDilZ0gJycnKQo='))
+import os
+w = '\x1b[1;37m'
+b = '\x1b[1;36m'
+g = '\x1b[1;32m'
+y = '\x1b[1;33m'
+r = '\x1b[1;31m'
+
+def banner():
+	os.system('clear')
+	print(f'''{g}╔╦╗┬ ┬┬ ┌┬┐┬   ╔╗ ╔═╗
+║║║│ ││  │ │───╠╩╗╠╣
+╩ ╩└─┘┴─┘┴ ┴   ╚═╝╚{y} v0.1
+{w}╔═════════════════════════════════════════╗
+{w}║{y}*{w} Author{r} :{b} Anbia Nuansyah             {w}   ║
+{w}║{y}*{w} Youtube{r}:{b} TANKERTV               {w}        ║
+{w}║{y}*{w} Github{r} :{g} \x1b[4mhttps://github.com/tankertv11\x1b[0m{w}    ║
+{w}╚═════════════════════════════════════════╝ ''')
